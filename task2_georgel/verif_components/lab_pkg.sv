36f77f68dfaeacefe01f309951576f75cee1280c
George <georgelin469@gmail.com>
2025-09-28 09:27:22 -0700

    Fixing typo in lab_pkg

--- task2_georgel/verif_components/lab_pkg.sv
+++ task2_georgel/verif_components/lab_pkg.sv
@@ -11,27 +11,28 @@ package lab_pkg;
     typedef bit allowed_states      [string];
     typedef bit allowed_transitions [string];
 
-        // Cannot declare associate arrays inside packages as entries can only be added at runtime
-        function allowed_states get_allowed_states();
-            allowed_states dict;
-            dict ["IDLE"     ] = 1;           
-            dict ["DRAW"     ] = 1;        
-            dict ["DONE"     ] = 1;        
+    // Cannot declare associate arrays inside packages as entries can only be added at runtime
+    function allowed_states get_allowed_states();
+        allowed_states dict;
+        dict ["IDLE"] = 1;           
+        dict ["DRAW"] = 1;        
+        dict ["DONE"] = 1;        
 
-            return dict;
-        endfunction
+        return dict;
+    endfunction
 
-        function allowed_transitions get_allowed_transitions();
-            allowed_transitions dict;
+    function allowed_transitions get_allowed_transitions();
+        allowed_transitions dict;
 
-            dict [{"IDLE",        " -> ",   "DEAL_P1"}] = 1;
-            dict [{"DRAW",        " -> ",   "DRAW"}]    = 1;
-            dict [{"DRAW",        " -> ",   "DONE"}]    = 1;
-            dict [{"DONE",        " -> ",   "IDLE"}]    = 1;
+        dict [{"IDLE",        " -> ",   "IDLE"}] = 1;
+        dict [{"IDLE",        " -> ",   "DRAW"}] = 1;
+        dict [{"DRAW",        " -> ",   "DRAW"}] = 1;
+        dict [{"DRAW",        " -> ",   "DONE"}] = 1;
+        dict [{"DONE",        " -> ",   "IDLE"}] = 1;
 
-            return dict;
+        return dict;
 
-        endfunction
+    endfunction
     // synthesis translate_on
 
 
