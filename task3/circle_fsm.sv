81b1d6cf460e4566b21f3fd1d5f6c79ffbcd4814
Karsten Kirby Uy <karsten.uy@gmail.com>
2025-10-02 10:08:11 -0700

    init fsm done

---
+++ task3/circle_fsm.sv
@@ -0,0 +1,144 @@
+
+module circle_fsm(
+    input logic clk, 
+    input logic rst_n, 
+    input logic [2:0] colour,
+    input logic [7:0] centre_x, 
+    input logic [6:0] centre_y,
+    input logic [7:0] radius,
+    input logic start,
+    input logic signed [8:0] curr_crit,
+    input logic fill_done,
+    input logic signed [8:0] offset_x,
+    input logic signed [8:0] offset_y,
+    output logic done,
+    output logic [2:0] vga_colour,
+    output logic draw_circle,
+    output logic [2:0] octant_sel,
+    output logic fill_start,
+    output logic x_load,
+    output logic y_load,
+    output logic crit_load,
+    output logic inc_y,
+    output logic dec_x,
+    output logic calc_crit
+);
+    // ---------------- PACKAGE IMPORTS ----------------
+    import lab_pkg::*;
+
+    // ---------------- STATE VARIABLES ----------------
+    circle_FSM_state state, next;
+
+    // ---------------- MAIN FSM PROCESS ----------------
+    always_ff @( posedge clk ) begin : STATE_FF
+        if (rst_n == 1'd0)
+            state <= CIRCLE_IDLE;
+        else 
+            state <= next;
+    end
+
+    always_comb begin : NEXT_STATE_LOGIC
+        case(state)
+            CIRCLE_IDLE  : next = (start == 1'd1) ? CIRCLE_LOAD : CIRCLE_IDLE;
+            CIRCLE_LOAD  : next = CIRCLE_BLACK;
+            CIRCLE_BLACK : next = (fill_done == 1'd1) ? CIRCLE_OCT1 : CIRCLE_BLACK;
+            CIRCLE_OCT1  : next = CIRCLE_OCT2;
+            CIRCLE_OCT2  : next = CIRCLE_OCT3;
+            CIRCLE_OCT3  : next = CIRCLE_OCT4;
+            CIRCLE_OCT4  : next = CIRCLE_OCT5;
+            CIRCLE_OCT5  : next = CIRCLE_OCT6;
+            CIRCLE_OCT6  : next = CIRCLE_OCT7;
+            CIRCLE_OCT7  : next = CIRCLE_OCT8;
+            CIRCLE_OCT8  : next = (offset_y <= offset_x) ? CIRCLE_OCT1 : CIRCLE_DONE;
+            CIRCLE_DONE  : next = (start == 1'd1) ? CIRCLE_DONE : CIRCLE_IDLE;
+            default      : next = CIRCLE_IDLE;
+        endcase
+    end
+
+    always_comb begin : STATE_OUTPUTS
+
+        done        = 1'd0;
+        vga_colour  = 3'd0;
+        draw_circle = 1'd0;
+        octant_sel  = 3'd0;
+        fill_start  = 1'd0;
+        x_load      = 1'd0;
+        y_load      = 1'd0;
+        crit_load   = 1'd0;
+        inc_y       = 1'd0;
+        dec_x       = 1'd0;
+        calc_crit   = 1'd0;
+
+        case(state)
+            CIRCLE_LOAD  : begin 
+                x_load = 1'd1;
+                y_load = 1'd1;
+                crit_load = 1'd1;
+            end
+            CIRCLE_BLACK : begin 
+                draw_circle = 1'd0;
+                fill_start = 1'd1;
+                vga_colour = 1'd0; // BLACK
+            end
+            CIRCLE_OCT1  : begin 
+                draw_circle = 1'd1;
+                vga_colour = colour;
+                octant_sel = 3'd0;
+            end
+            CIRCLE_OCT2  : begin 
+                draw_circle = 1'd1;
+                vga_colour = colour;
+                octant_sel = 3'd1;
+            end
+            CIRCLE_OCT3  : begin 
+                draw_circle = 1'd1;
+                vga_colour = colour;
+                octant_sel = 3'd2;
+            end
+            CIRCLE_OCT4  : begin 
+                draw_circle = 1'd1;
+                vga_colour = colour;
+                octant_sel = 3'd3;
+            end
+            CIRCLE_OCT5  : begin 
+                draw_circle = 1'd1;
+                vga_colour = colour;
+                octant_sel = 3'd4;
+            end
+            CIRCLE_OCT6  : begin 
+                draw_circle = 1'd1;
+                vga_colour = colour;
+                octant_sel = 3'd5;
+            end
+            CIRCLE_OCT7  : begin 
+                draw_circle = 1'd1;
+                vga_colour = colour;
+                octant_sel = 3'd6;
+            end
+            CIRCLE_OCT8  : begin 
+                draw_circle = 1'd1;
+                vga_colour = colour;
+                octant_sel = 3'd7;
+                inc_y = 1'd1;
+                if (curr_crit > 0)
+                    dec_x = 1'd1;
+                calc_crit = 1'd1;
+            end
+            CIRCLE_DONE  : done = 1'd1;
+            default      : begin
+                done        = 1'd0;
+                vga_colour  = 3'd0;
+                draw_circle = 1'd0;
+                octant_sel  = 3'd0;
+                fill_start  = 1'd0;
+                x_load      = 1'd0;
+                y_load      = 1'd0;
+                crit_load   = 1'd0;
+                inc_y       = 1'd0;
+                dec_x       = 1'd0;
+                calc_crit   = 1'd0;
+            end
+        endcase
+    end
+
+endmodule
--- task3/verif_components/lab_pkg.sv
+++ task3/verif_components/lab_pkg.sv
@@ -1,37 +1,65 @@
 package lab_pkg;
-    `define FSM_SWIDTH 3 // Keep large enough for a one-hot encoded FSM
 
-    typedef enum logic [`FSM_SWIDTH:0] { 
-        IDLE,          // FSM entry point
-        CLEAR_SCREEN,  // Set screen to black
-        DRAW,          // Drawing loop
-        DONE           // Assert done signal
-    } e_FSM_state;
+    ///////////////////////////////////////////////////////////
+    // circle assets
+
+    `define FSM_SWIDTH_CIRCLE 4
+
+    typedef enum logic [`FSM_SWIDTH_CIRCLE-1:0] { 
+        CIRCLE_IDLE,        
+        CIRCLE_LOAD,        
+        CIRCLE_BLACK,
+        CIRCLE_OCT1,         
+        CIRCLE_OCT2,         
+        CIRCLE_OCT3,         
+        CIRCLE_OCT4,         
+        CIRCLE_OCT5,         
+        CIRCLE_OCT6,         
+        CIRCLE_OCT7,         
+        CIRCLE_OCT8,         
+        CIRCLE_DONE         
+    } circle_FSM_state;
+
+    ///////////////////////////////////////////////////////////
+    // fillscreen assets
+    `define FSM_SWIDTH_FILLSCREEN 2
+  
+    typedef enum logic [`FSM_SWIDTH_FILLSCREEN-1:0] { 
+        FILL_IDLE,        // FSM entry point
+        FILL_DRAW,        // Draw 
+        FILL_DONE         // assert the done signal
+    } fillscreen_FSM_state;
 
     // synthesis translate_off
     typedef bit allowed_states      [string];
     typedef bit allowed_transitions [string];
 
     // Cannot declare associate arrays inside packages as entries can only be added at runtime
-    function allowed_states get_allowed_states();
+    function allowed_states get_allowed_states_fillscreen();
         allowed_states dict;
-        dict ["IDLE"     ] = 1;           
-        dict ["DRAW"     ] = 1;        
-        dict ["DONE"     ] = 1;        
+        dict ["FILL_IDLE"     ] = 1;           
+        dict ["FILL_DRAW"     ] = 1;        
+        dict ["FILL_DONE"     ] = 1;     
 
         return dict;
+
     endfunction
 
-    function allowed_transitions get_allowed_transitions();
+    function allowed_transitions get_allowed_transitions_fillscreen();
+
         allowed_transitions dict;
 
-        dict [{"IDLE",         " -> ",   "CLEAR_SCREEN"}] = 1;
-        dict [{"CLEAR_SCREEN", " -> ",   "DRAW"}]         = 1;
-        dict [{"DRAW",         " -> ",   "DONE"}]         = 1;
+        dict [{"FILL_IDLE",        " -> ",   "FILL_DRAW"}]  = 1;
+        dict [{"FILL_DRAW",        " -> ",   "FILL_DONE"}]  = 1;
+        dict [{"FILL_DONE",        " -> ",   "FILL_IDLE"}]  = 1;
+        dict [{"FILL_IDLE",        " -> ",   "FILL_IDLE"}]  = 1;
+        dict [{"FILL_DONE",        " -> ",   "FILL_DONE"}]  = 1;
+        dict [{"FILL_DRAW",        " -> ",   "FILL_DRAW"}]  = 1;
 
         return dict;
 
     endfunction
-// synthesis translate_on
+    // synthesis translate_on
+
 
 endpackage
