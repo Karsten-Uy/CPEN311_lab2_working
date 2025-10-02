514a7bee66fa0452be9e66f616d7426ac4f155d9
Karsten Kirby Uy <karsten.uy@gmail.com>
2025-10-02 10:38:45 -0700

    init circle

--- task3/circle.sv
+++ task3/circle.sv
@@ -4,5 +4,57 @@ module circle(input logic clk, input logic rst_n, input logic [2:0] colour,
               output logic [7:0] vga_x, output logic [6:0] vga_y,
               output logic [2:0] vga_colour, output logic vga_plot);
      // draw the circle
+
+     datapath DP(
+          .clk         (clk),
+          .resetn      (resetn),
+          .radius      (radius),
+          .centre_x    (centre_x),
+          .centre_y    (centre_y),
+
+          .fill_start  (fill_start),
+          .fill_done   (fill_done),
+          .draw_circle (draw_circle),
+          .octant_sel  (octant_sel),
+          .dec_x       (dec_x),
+          .inc_y       (inc_y),
+          .calc_crit   (calc_crit),
+          .load_x      (load_x),
+          .load_y      (load_y),
+          .load_crit   (load_crit),
+          .offset_x    (offset_x),
+          .offset_y    (offset_y),
+          .crit        (crit),
+          
+          .vga_x       (vga_x),
+          .vga_y       (vga_y),
+          .plot        (vga_plot)
+     );
+
+     circle_fsm CIRCLE_FSM(
+          .clk         (clk),
+          .rst_n       (rst_n),
+          .colour      (colour),
+          .centre_x    (centre_x),
+          .centre_y    (centre_y),
+          .radius      (radius),
+          .start       (start),
+          .curr_crit   (curr_crit),
+          .fill_done   (fill_done),
+          .offset_x    (offset_x),
+          .offset_y    (offset_y),
+          .done        (done),
+          .vga_colour  (vga_colour),
+          .draw_circle (draw_circle),
+          .octant_sel  (octant_sel),
+          .fill_start  (fill_start),
+          .x_load      (x_load),
+          .y_load      (y_load),
+          .crit_load   (crit_load),
+          .inc_y       (inc_y),
+          .dec_x       (dec_x),
+          .calc_crit   (calc_crit)
+     );
+
 endmodule
 
--- task3/task3.sv
+++ task3/task3.sv
@@ -9,4 +9,53 @@ module task3(input logic CLOCK_50, input logic [3:0] KEY,
 
     // instantiate and connect the VGA adapter and your module
 
+    // NEED TO ADD THESE TO GET IT TO WORK ON DE1_SoC
+    logic [9:0] VGA_R_10;
+    logic [9:0] VGA_G_10;
+    logic [9:0] VGA_B_10;
+    logic VGA_BLANK, VGA_SYNC;
+    assign VGA_R = VGA_R_10[9:2];
+    assign VGA_G = VGA_G_10[9:2];
+    assign VGA_B = VGA_B_10[9:2];
+
+    logic [7:0] vga_x;
+    logic [6:0] vga_y;
+    logic [2:0] vga_colour;
+    logic vga_plot;
+
+    circle CIRCLE(
+        .clk        (CLOCK_50),
+        .rst_n      (KEY[3]),
+        .colour     (3'd2),
+        .centre_x   (8'd80),
+        .centre_y   (8'd60),
+        .radius     (8'd40),
+        .start      (KEY[0]),
+        .done       (LEDR[0]),
+        .vga_x      (vga_x),
+        .vga_y      (vga_y),
+        .vga_colour (vga_colour),
+        .vga_plot   (vga_plot)
+    );
+
+    // Internal Outputs
+    assign VGA_X      = vga_x;
+    assign VGA_Y      = vga_y;
+    assign VGA_COLOUR = vga_colour;
+    assign VGA_PLOT   = vga_plot;
+
+    vga_adapter#(        
+        .RESOLUTION("160x120")) vga_u0(
+        .resetn(KEY[3]), 
+        .clock(CLOCK_50), 
+        .colour(vga_colour),
+        .x(vga_x), 
+        .y(vga_y), 
+        .plot(vga_plot),
+        .VGA_R(VGA_R_10), 
+        .VGA_G(VGA_G_10), 
+        .VGA_B(VGA_B_10),
+        .*);
+
+
 endmodule: task3
